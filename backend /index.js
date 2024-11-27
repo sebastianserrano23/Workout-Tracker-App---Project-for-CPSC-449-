c4bb293119c7c84e20160198e749726704175b18
Sebastian <serranosebastian23@gmail.com>
2024-11-27 06:58:03 -0800

    connected to the database

--- backend /index.js
+++ backend /index.js
@@ -1,14 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => {
-    console.log('App is listening on Port 3000');
-});
+const MONGO_URL = process.env.MONGO_URL;
 
 app.get('/', (req, res)=>{
     try{
@@ -18,3 +16,15 @@ app.get('/', (req, res)=>{
         res.status(404).send('Something went wrong! :(')
     }
 });
+
+mongoose
+    .connect(MONGO_URL)
+    .then(() => {
+        console.log("Connected to MongoDB!")
+        app.listen(PORT, () => {
+            console.log('App is listening on Port 3000.');
+        });
+    })
+    .catch((error) => {
+        console.log(error);
+    });
