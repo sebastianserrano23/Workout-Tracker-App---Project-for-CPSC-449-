a86e29e27cd2024f61aedf5b8df714c2e24e5b25
Sebastian <serranosebastian23@gmail.com>
2024-11-28 12:48:44 -0800

    modification to member schema

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Workout-Tracker-App---Project-for-CPSC-449-
-App using the mern stack (MongoDB, Express, React.js and Node) for Backend Engineering (CPSC 449)
+App using the mern stack (MongoDB, Express, React.js and Node) for my Backend Engineering class (CPSC 449)
--- backend /models/members.js
+++ backend /models/members.js
@@ -1,9 +1,25 @@
 import mongoose from "mongoose";
 
-const member_information = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true}
-})
+const memberSchema = new Schema({
+    firstname: {
+        type: String,
+        required: true
+    },
+    lastname: {
+        type: String,
+        required: true
+    },
+    username: { 
+        type: String, required: true 
+    },
+    email: { 
+        type: String, required: true 
+    },
+    password: { 
+        type: String, required: true
+    }
+});
 
-export default member_information;
+const Member = model("Member", memberSchema)
+
+export default Member;
